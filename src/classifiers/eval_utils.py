a88f5663ad19ac6c6f75073d77b194f2481d0e2d
yiyi chen <chen.yiyi@protonmail.com>
2025-02-03 21:41:20 +0100

    revise

--- src/classifiers/eval_utils.py
+++ src/classifiers/eval_utils.py
@@ -18,12 +18,14 @@ def eval_classification(references, output, classification):
         auc_results = auc_metric.compute(references=references, prediction_scores=output,
                                         multi_class="ovo")
     else:
+        positive_class_probs = output[:,1]
         auc_metric = evaluate.load("roc_auc")
-        auc_results = auc_metric.compute(references=references, prediction_scores=output)
+        auc_results = auc_metric.compute(references=references, prediction_scores=positive_class_probs)
 
     auc_score = round(auc_results["roc_auc"], 4)*100
 
-    f1_result = f1_metric.compute(predictions=predictions, references=references, average="macro")
+    f1_result = f1_metric.compute(predictions=predictions, references=references,
+                                  average="macro" if classification=="multiclass" else "binary")
     f1_score = round(f1_result["f1"], 4)*100
 
     acc_result = accuracy_metric.compute(references=references, predictions=predictions)
