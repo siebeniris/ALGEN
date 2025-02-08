becfa70ad8c147324f37a85983909748490e9499
yiyi chen <chen.yiyi@protonmail.com>
2025-02-08 14:30:51 +0100

    defense batch

--- lumi_defense_downstream_batch.sh
+++ lumi_defense_downstream_batch.sh
@@ -8,8 +8,11 @@ sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google/mt5-base 128 WET 0 0 0
 
 sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google-bert/bert-base-multilingual-cased 128 WET 0 0 0
 
-
 sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 sentence-transformers/gtr-t5-base 128 WET 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 text-embedding-ada-002 128 WET 0 0 0
+
+
 
 # ========
 sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-t5/t5-base 128 WET 0 0 0
@@ -21,6 +24,10 @@ sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-bert/bert-base-multi
 
 
 sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 sentence-transformers/gtr-t5-base 128 WET 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 text-embedding-ada-002 128 WET 0 0 0
+
 
 # ========
 
@@ -35,6 +42,8 @@ sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 google-bert/bert-base-multi
 
 sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 sentence-transformers/gtr-t5-base 128 WET 0 0 0
 
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 text-embedding-ada-002 128 WET 0 0 0
+
 
 # Shuffling
 
@@ -48,6 +57,8 @@ sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google-bert/bert-base-multilingua
 
 sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 sentence-transformers/gtr-t5-base 128 Shuffling 0 0 0
 
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 text-embedding-ada-002 128 Shuffling 0 0 0
+
 # ========
 sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-t5/t5-base 128 Shuffling 0 0 0
 
@@ -56,8 +67,9 @@ sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google/mt5-base 128 Shuffli
 
 sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-bert/bert-base-multilingual-cased 128 Shuffling 0 0 0
 
-
 sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 sentence-transformers/gtr-t5-base 128 Shuffling 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 text-embedding-ada-002 128 Shuffling 0 0 0
 
 # ========
 
@@ -71,3 +83,54 @@ sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 google-bert/bert-base-multi
 
 
 sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 sentence-transformers/gtr-t5-base 128 Shuffling 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 text-embedding-ada-002 128 Shuffling 0 0 0
+
+
+
+
+# NoDefense
+
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google-t5/t5-base 128 NoDefense 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google/mt5-base 128 NoDefense 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 google-bert/bert-base-multilingual-cased 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 sentence-transformers/gtr-t5-base 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/snli_ds nli 3 text-embedding-ada-002 128 NoDefense 0 0 0
+
+
+# ========
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-t5/t5-base 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google/mt5-base 128 NoDefense 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 google-bert/bert-base-multilingual-cased 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 sentence-transformers/gtr-t5-base 128 NoDefense 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/sst2_ds sentiment 2 text-embedding-ada-002 128 NoDefense 0 0 0
+
+
+# ========
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 google-t5/t5-base 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 google/mt5-base 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 google-bert/bert-base-multilingual-cased 128 NoDefense 0 0 0
+
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 sentence-transformers/gtr-t5-base 128 NoDefense 0 0 0
+
+sbatch lumi_classifiers.sh yiyic/s140_ds sentiment 2 text-embedding-ada-002 128 NoDefense 0 0 0
+
