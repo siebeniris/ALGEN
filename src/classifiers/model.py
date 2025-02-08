b8600cf4612a464ce9e279b92179c159daa8c6ec
yiyi chen <chen.yiyi@protonmail.com>
2025-02-08 15:02:03 +0100

    classifiers

--- src/classifiers/model.py
+++ src/classifiers/model.py
@@ -13,76 +13,63 @@ class Classifier(nn.Module):
 
         # purmech, lapmech3
         # https://github.com/xiangyue9607/Sentence-LDP/blob/main/model.py
-        if self.defense_method in ["PurMech", "LapMech"]:
-            self.dropout = nn.Dropout(p=dropout_rate)
-            if self.input_dim > 768:
-                self.project_1 = nn.Linear(input_dim, 1024)
-                self.project_2 = nn.Linear(1024, self.proj_dim)
-                self.project_3 = nn.Linear(self.proj_dim, 1024)
-                self.project_4 = nn.Linear(1024, input_dim)
-            else:
-
-                self.project_1 = nn.Linear(input_dim, self.proj_dim)
-                self.project_2 = nn.Linear(self.proj_dim, input_dim)
-                self.activation = nn.Tanh()
-            self.classifier = nn.Linear(input_dim, num_labels)
+        # if self.defense_method in ["PurMech", "LapMech"]:
+        self.dropout = nn.Dropout(p=dropout_rate)
+        if self.input_dim > 768:
+            self.project_1 = nn.Linear(input_dim, 1024)
+            self.project_2 = nn.Linear(1024, self.proj_dim)
+            self.project_3 = nn.Linear(self.proj_dim, 1024)
+            self.project_4 = nn.Linear(1024, input_dim)
         else:
-            if self.input_dim > 768:
-                self.fc1 = nn.Linear(input_dim, 1024)  # Increased hidden units for larger embeddings
-                self.fc2 = nn.Linear(1024, num_labels)
-                self.relu = nn.ReLU()
-                self.dropout = nn.Dropout(p=dropout_rate)
-                # Layer for smaller input (e.g., BERT embeddings)
-            else:
-                self.dropout = nn.Dropout(p=dropout_rate)
-                self.fc = nn.Linear(input_dim, num_labels)
+            self.project_1 = nn.Linear(input_dim, self.proj_dim)
+            self.project_2 = nn.Linear(self.proj_dim, input_dim)
+        self.activation = nn.Tanh()
+        self.classifier = nn.Linear(input_dim, num_labels)
+        # else:
+        #     self.dropout = nn.Dropout(p=dropout_rate)
+        #     if self.input_dim > 768:
+        #         self.fc1 = nn.Linear(input_dim, 1024)  # Increased hidden units for larger embeddings
+        #         self.relu = nn.ReLU()
+        #         self.fc2 = nn.Linear(1024, num_labels)
+        #     else:
+        #         self.fc = nn.Linear(input_dim, num_labels)
 
     def forward(self, x):
-        if self.defense_method in ["PurMech", "LapMech"]:
-            x = self.dropout(x)
+        # if self.defense_method in ["PurMech", "LapMech"]:
+        x = self.dropout(x)
 
-            if self.input_dim > 768:
-                x = self.project_1(x)
-                x = self.project_2(x)
-                x = self.activation(x)
-                if self.defense_method == "PurMech":
-                    x = PurMech(x, self.epsilon)
-                elif self.defense_method == "LapMech":
-                    x = LapMech(x, self.epsilon)
-                x = self.project_3(x)
-                x = self.project_4(x)
-            else:
-                x = self.project_1(x)
-                x = self.activation(x)
-                if self.defense_method == "PurMech":
-                    x = PurMech(x, self.epsilon)
-                elif self.defense_method == "LapMech":
-                    x = LapMech(x, self.epsilon)
-                x = self.project_2(x)
-
+        if self.input_dim > 768:
+            x = self.project_1(x)
+            x = self.project_2(x)
             x = self.activation(x)
-            x = self.classifier(x)
-            return x
+            if self.defense_method == "PurMech":
+                x = PurMech(x, self.epsilon)
+            elif self.defense_method == "LapMech":
+                x = LapMech(x, self.epsilon)
+            x = self.project_3(x)
+            x = self.project_4(x)
         else:
-            x = self.dropout(x)
-
-            if self.input_dim > 768:
-                # print("Using the model specific for GPT embeddings")
-                x = self.project_1(x)
-                x = self.project_2(x)
-                x = self.activation(x)
-                x = self.project_3(x)
-                x = self.project_4(x)
-                # x = self.dropout(x)
-                # x = self.fc1(x)
-                # x = self.relu(x)
-                # x = self.fc2(x)
-                return x
-            else:
-                x = self.project_1(x)
-                x = self.activation(x)
-                x = self.project_2(x)
-
+            x = self.project_1(x)
             x = self.activation(x)
-            x = self.classifier(x)
-            return x
+            if self.defense_method == "PurMech":
+                x = PurMech(x, self.epsilon)
+            elif self.defense_method == "LapMech":
+                x = LapMech(x, self.epsilon)
+            x = self.project_2(x)
+
+        x = self.activation(x)
+        x = self.classifier(x)
+        return x
+    # else:
+    #     x = self.dropout(x)
+    #
+    #     if self.input_dim > 768:
+    #         # print("Using the model specific for GPT embeddings")
+    #
+    #         x = self.fc1(x)
+    #         x = self.relu(x)
+    #         x = self.fc2(x)
+    #         return x
+    #     else:
+    #         x = self.fc(x)
+    #         return x
